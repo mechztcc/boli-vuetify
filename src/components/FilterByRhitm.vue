f8d808089e9ea4c038df776a8e15ab5034a006b3
mechztcc <dojthan@gmail.com>
2023-07-13 11:31:12 -0300

    feat: filter by rhitm

---
+++ src/components/FilterByRhitm.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="rounded-xl pa-5">
+    <span>Ritmo</span>
+
+    <v-text-field label="Procure por modalidade(s) de dança" variant="solo" class="mt-3"></v-text-field>
+  </v-card>
+</template>
+<script lang="ts">
+export default {
+  name: "filter-by-rhitm-component",
+};
+</script>
+<style lang=""></style>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -8,18 +8,23 @@
     <v-col cols="12" md="4" lg="4">
       <FilterByType />
     </v-col>
+    <v-col cols="12" md="4" lg="4">
+      <FilterByRhitm />
+    </v-col>
   </v-container>
 </template>
 <script lang="ts">
 import AppBar from "../components/Appbar.vue";
 import FilterByDate from "../components/FilterByDate.vue";
 import FilterByType from "../components/FilterByType.vue";
+import FilterByRhitm from "../components/FilterByRhitm.vue";
 export default {
   name: "home-layout",
   components: {
     AppBar,
     FilterByDate,
-    FilterByType
+    FilterByType,
+    FilterByRhitm
   },
 };
 </script>
