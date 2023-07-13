db6e51b298eb1cac28ddee7c97190452211e6783
mechztcc <dojthan@gmail.com>
2023-07-13 11:10:59 -0300

    feat: created app bar

---
+++ src/components/Appbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid class="ma-0 pa-0">
+    <v-col cols="12" class="d-flex justify-space-between pa-0">
+      <v-col cols="2" md="2" class="d-flex justify-center align-center">
+        <img src="@/assets/Logo.svg" alt="" />
+      </v-col>
+      <v-col
+        cols="4"
+        md="6"
+        class="ma-0 d-flex justify-space-center align-center"
+      >
+        <v-text-field
+          label="Pesquise por modalidade ou @professor"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo"
+          rounded="lg"
+          class="ma-0"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2" class="d-flex justify-center align-center">
+        <h4 class="mx-5">Fazer login</h4>
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-col>
+    </v-col>
+  </v-container>
+</template>
+<script lang="ts">
+export default {
+  name: "appbar-component",
+};
+</script>
+<style lang="scss">
+img {
+  width: clamp(40px, 80px, 80px);
+  height: clamp(40px, 80px, 80px);
+}
+</style>
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <AppBar />
+</template>
+<script lang="ts">
+import AppBar from "../components/Appbar.vue";
+export default {
+  name: "home-layout",
+  components: {
+    AppBar,
+  },
+};
+</script>
+<style lang=""></style>
