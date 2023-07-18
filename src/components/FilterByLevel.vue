dd247487a4c3317a6024ba04f49dd0a784c57936
mechztcc <dojthan@gmail.com>
2023-07-18 14:52:57 -0300

    feat: added filter by level

--- src/components/FilterByLevel.vue
+++ src/components/FilterByLevel.vue
@@ -2,15 +2,53 @@
   <v-card class="pa-5 rounded-xl mt-3">
     <span>Nível</span>
 
-    <v-checkbox class="mt-4" label="Para todos" color="primary"></v-checkbox>
-    <v-checkbox label="Iniciante" color="primary"></v-checkbox>
-    <v-checkbox label="Intermediário" color="primary"></v-checkbox>
-    <v-checkbox label="Avançado" color="primary"></v-checkbox>
+    <v-checkbox
+      class="mt-4"
+      label="Para todos"
+      color="primary"
+      value="1"
+      @click="onSelectLevel($event.target['value'])"
+    ></v-checkbox>
+    <v-checkbox
+      label="Iniciante"
+      color="primary"
+      value="2"
+      @click="onSelectLevel($event.target['value'])"
+    ></v-checkbox>
+    <v-checkbox
+      label="Intermediário"
+      color="primary"
+      value="3"
+      @click="onSelectLevel($event.target['value'])"
+    ></v-checkbox>
+    <v-checkbox
+      label="Avançado"
+      color="primary"
+      value="4"
+      @click="onSelectLevel($event.target['value'])"
+    ></v-checkbox>
   </v-card>
 </template>
 <script lang="ts">
+import { useSearchStore } from "../store/search";
 export default {
   name: "filter-by-level-component",
+  data() {
+    return {
+      store: useSearchStore(),
+    };
+  },
+  methods: {
+    onSelectLevel(value: number) {
+      if (this.store.$state.level.includes(value)) {
+        this.store.$state.level = this.store.$state.level.filter((el) => {
+          return el != value;
+        });
+      } else {
+        this.store.$state.level.push(value);
+      }
+    },
+  },
 };
 </script>
 <style lang=""></style>
