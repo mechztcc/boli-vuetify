1f47c1cd435ddf0b5e0a7cd8e0346df07cc393d1
mechztcc <dojthan@gmail.com>
2023-07-18 16:30:15 -0300

    feat: added image on card lesson

--- src/components/CardLesson.vue
+++ src/components/CardLesson.vue
@@ -4,41 +4,48 @@
       <v-row>
         <v-col cols="12" md="12" lg="5">
           <div class="d-flex align-center justify-center">
-            <img class="rounded-xl" src="@/assets/photo.jpeg" alt="" />
+            <img class="rounded-xl" v-bind:src="info?.image" alt="" />
             <div class="d-flex flex-column mx-5">
               <h4 class="text-primary">{{ info?.name }}</h4>
               <small class="subtitle">{{ day }}</small>
-              <small class="subtitle">{{ info?.inicialTime }} {{ info?.finalTime }}</small>
+              <small class="subtitle"
+                >{{ info?.inicialTime }} {{ info?.finalTime }}</small
+              >
               <span>{{ info?.teacherName }}</span>
               <span class="text-secondary">@{{ info?.teacherNickname }}</span>
             </div>
           </div>
         </v-col>
+
         <v-col cols="12" md="12" lg="7">
-          <div class="d-flex align-center justify-center h-100">
+          <div class="d-flex align-center justify-space-around h-100">
             <div class="d-flex flex-column align-center justify-space-center">
               <span>Nível</span>
               <span class="bg-primary rounded-xl px-2">{{ info?.level }}</span>
             </div>
-
-            <div
-              class="d-flex flex-column align-center justify-space-center mx-5"
-            >
-              <span>Tipo</span>
-              <span class="bg-primary rounded-xl px-2">{{
-                info?.location
-              }}</span>
-            </div>
-
             <div class="d-flex flex-column align-center justify-space-center">
               <span>Preço</span>
               <span class="rounded-xl px-2">{{ info?.credit }} crédito(s)</span>
             </div>
+          </div>
+        </v-col>
+      </v-row>
 
-            <div class="d-flex mx-5 align-center justify-space-center">
-              <span>Ver aula</span>
-              <v-icon>mdi-chevron-right</v-icon>
-            </div>
+      <v-row>
+        <v-col cols="12" md="6" lg="4">
+          <div class="d-flex flex-column align-center justify-center">
+            <span class="mt-2">Tipo</span>
+            <small class="bg-primary rounded-xl px-2 py-1">
+              {{ info?.location }}
+            </small>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" lg="4">
+          <div class="d-flex flex-column align-center justify-center">
+            <span class="mt-2">Modalidade</span>
+            <small class="bg-primary rounded-xl px-2 py-1">
+              {{ info?.modality }}
+            </small>
           </div>
         </v-col>
       </v-row>
