73a4e6efabc3f4d8540ed7250bbfcf7bd3920dcf
mechztcc <dojthan@gmail.com>
2023-07-20 10:33:00 -0300

    feat: clear date picker button

--- src/components/FilterByDate.vue
+++ src/components/FilterByDate.vue
@@ -9,17 +9,29 @@
       :enable-time-picker="false"
       auto-apply
     ></VueDatePicker>
+    <div class="text-end">
+      <v-btn
+        outline
+        color="primary"
+        size="x-small"
+        class="mt-2"
+        rounded="xl"
+        @click="date = '0000-00-00'"
+      >
+        <v-icon color="white">mdi-delete-forever</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 <script lang="ts">
+import { reverseDate } from "../services/handle-dates";
 import { useSearchStore } from "../store/search";
-import { addMaskDate, reverseDate } from "../services/handle-dates";
 
 export default {
   name: "filter-by-date-component",
   data() {
     return {
-      date: '0000-00-00',
+      date: "0000-00-00",
       store: useSearchStore(),
     };
   },
@@ -27,7 +39,14 @@ export default {
   computed: {},
   watch: {
     date() {
-
+      if (this.date === "0000-00-00") {
+        const state = this.store.$state;
+        this.store.$state = {
+          ...state,
+          day: this.date,
+        };
+        return;
+      }
       const date = new Date(this.date).toLocaleDateString();
 
       const state = this.store.$state;
