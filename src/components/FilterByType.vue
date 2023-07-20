573439ecc74f49a11821a2b6a8617e53d0343527
mechztcc <dojthan@gmail.com>
2023-07-20 09:38:07 -0300

    feat: added pagination

--- src/components/FilterByType.vue
+++ src/components/FilterByType.vue
@@ -13,10 +13,26 @@
     ></v-checkbox>
 
     <v-text-field
-      label="Insira seu endereço"
+      label="Cidade"
       variant="plain"
       rounded="xl"
+      v-model="location.city"
+      v-if="status.local"
+      class="fadeIn"
     ></v-text-field>
+
+    <v-text-field
+      label="Bairro"
+      variant="plain"
+      rounded="xl"
+      v-model="location.district"
+      v-if="status.local"
+      class="fadeIn"
+    ></v-text-field>
+
+    <v-btn round color="primary" class="w-100" rounded="xl" @click="onSearch()">
+      <span class="text-white"> Buscar </span>
+    </v-btn>
   </v-card>
 </template>
 <script lang="ts">
@@ -29,6 +45,10 @@ export default {
       status: {
         online: true,
         local: false,
+      },
+      location: {
+        city: "",
+        district: "",
       },
     };
   },
@@ -44,6 +64,16 @@ export default {
         };
       },
       deep: true,
+    },
+  },
+
+  methods: {
+    onSearch() {
+      this.store.$state = {
+        ...this.store.$state,
+        city: this.location.city,
+        district: this.location.district,
+      };
     },
   },
 };
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -18,8 +18,16 @@
         <FilterByLevel />
       </v-col>
       <v-col cols="12" md="8" lg="8" v-if="!isLoading && data.length > 0">
-        <CardLesson v-for="(item, index) in paginate" :key="index" :info="item" />
-        <v-pagination :length="data.length / itemsPerPage"></v-pagination>
+        <CardLesson
+          v-for="(item, index) in filtered"
+          :key="index"
+          :info="item"
+        />
+        <v-pagination
+          v-model="page"
+          :length="data.length / itemsPerPage"
+          v-on:update:model-value="paginate()"
+        ></v-pagination>
       </v-col>
       <v-col cols="12" md="8" lg="8" v-if="isLoading">
         <div class="d-flex h-100 justify-center align-center">
@@ -51,6 +59,7 @@ import { useSearchStore } from "../store/search";
 import { useHomeStore } from "../store/home";
 import { search } from "../services/http.service";
 import { mapState } from "pinia";
+import { ILesson } from "@/types/lesson.interface";
 
 export default {
   name: "home-layout",
@@ -69,8 +78,9 @@ export default {
     return {
       store: useSearchStore(),
       storeHome: useHomeStore(),
-      items: [],
       itemsPerPage: 5,
+      page: 1,
+      filtered: [] as ILesson[],
       isLoading: false,
     };
   },
@@ -79,16 +89,26 @@ export default {
     this.store.$subscribe((mutation, state) => {
       this.isLoading = true;
       search(state)
-        .then((data) => (this.storeHome.$state.data = data))
+        .then(
+          (data) => (
+            (this.storeHome.$state.data = data),
+            (this.filtered = data?.slice(0, this.itemsPerPage))
+          )
+        )
         .finally(() => (this.isLoading = false));
     });
   },
 
   computed: {
     ...mapState(useHomeStore, ["data"]),
+  },
+
+  methods: {
     paginate() {
-      return this.data.slice(0, this.itemsPerPage - 1);
+      const offset = this.page * this.itemsPerPage - this.page;
+      this.filtered = this.data.slice(offset, offset + this.itemsPerPage);
     },
+
   },
 
   watch: {},
