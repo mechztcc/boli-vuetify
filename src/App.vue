bc2a20b4b6b7e852234c7c605e6245bb916da368
mechztcc <dojthan@gmail.com>
2023-07-24 12:06:03 -0300

    feat: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <router-view />
+  <Footer />
 </template>
 
-<script lang="ts" setup>
-//
+<script lang="ts">
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+};
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex bg-primary pa-10 mt-10">
+    <span class="text-white"> © 2021 Boli Todos os direitos reservados </span>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "footer-component",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang=""></style>
